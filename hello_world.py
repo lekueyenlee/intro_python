b971e8abe1c2735095352d2089acedb46c420ccb
lekueyenlee <lekueyen.lee@gmail.com>
2020-04-20 06:27:02 -0500

    Update hello_world.py
    Added in comment and variable example in the code

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,11 @@
+### Description: Example of print statement and using variable
+### 
+###
+
+# Example 1 - Print statement
 print("Hello World")
+
+
+# Example 2 - declared a variable and output value
+word = "Hello World"
+print(word)
