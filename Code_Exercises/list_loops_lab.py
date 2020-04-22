b53c957858f53333c53afc038fa1c3a7be303d80
lekueyenlee <lekueyen.lee@gmail.com>
2020-04-21 23:21:00 -0500

    Added in list loops lab
    Added in list/loops lab exercise

---
+++ Code_Exercises/list_loops_lab.py
@@ -0,0 +1,13 @@
+#
+# List, For Loop, and While Loop
+
+my_food_list = ['pizza', 'noodles', 'rice']
+
+for f in my_food_list:
+    print(f)
+
+i = 0
+
+while i < len(my_food_list):
+    print(my_food_list[i]) #loops through each element in the collection
+    i = i + 1
