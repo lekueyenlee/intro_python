6f64bf661eb2063613067a163b9341e573d2d3cf
lekueyenlee <lekueyen.lee@gmail.com>
2020-04-23 13:15:25 -0500

    Create method_code_challenge.py
    Create and add the the code challenge for method

---
+++ Code_Exercises/method_code_challenge.py
@@ -0,0 +1,14 @@
+# method code challenge
+#
+
+user_input_name = input("What is your name? ")
+user_input_contact_info = input("What is your contact infor? ")
+
+def show_my_info(in_name, in_contact_info):
+    print("\n")
+    print("!----------------------  Your Information  ----------------------!")
+    print("Name: " + in_name)
+    print("Contact: " + in_contact_info)
+
+show_my_info(user_input_name, user_input_contact_info)
+
