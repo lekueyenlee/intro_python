73a736f07ddb03c3cae2ac302321cf2a7d852c31
lekueyenlee <lekueyen.lee@gmail.com>
2020-04-24 14:30:18 -0500

    Update method_code_challenge.py
    Added in the few sample lines of code.

--- Code_Exercises/method_code_challenge.py
+++ Code_Exercises/method_code_challenge.py
@@ -2,13 +2,15 @@
 #
 
 user_input_name = input("What is your name? ")
-user_input_contact_info = input("What is your contact infor? ")
+user_input_contact_info = input("What is your contact info? ")
+user_id = input("What is your user ID?")
 
-def show_my_info(in_name, in_contact_info):
+def show_my_info(in_name, in_contact_info, in_user_id):
     print("\n")
     print("!----------------------  Your Information  ----------------------!")
+    print("User ID " + in_user_id)
     print("Name: " + in_name)
     print("Contact: " + in_contact_info)
 
-show_my_info(user_input_name, user_input_contact_info)
+show_my_info(user_input_name, user_input_contact_info, user_id)
 
