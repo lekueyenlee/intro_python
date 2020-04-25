84d45f1a9006272a3ea20ee2030e265e5bab7d3b
lekueyenlee <lekueyen.lee@gmail.com>
2020-04-25 11:14:08 -0500

    Update method_code_challenge.py
    Added in the sys.exit() method

--- Code_Exercises/method_code_challenge.py
+++ Code_Exercises/method_code_challenge.py
@@ -1,6 +1,8 @@
 # method code challenge
 #
 
+import sys
+
 user_input_name = input("What is your name? ")
 user_input_contact_info = input("What is your contact info? ")
 user_id = input("What is your user ID?")
@@ -13,4 +15,4 @@ def show_my_info(in_name, in_contact_info, in_user_id):
     print("Contact: " + in_contact_info)
 
 show_my_info(user_input_name, user_input_contact_info, user_id)
-
+sys.exit()
